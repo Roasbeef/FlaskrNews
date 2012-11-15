58c78ce97fa5b00166ba06d995f2947a8e593022
Olaoluwa Osuntokun <laolu32@gmail.com>
2012-11-14 18:28:10 -0800

    Folding comments bug fix/refactor

--- app/static/js/comment_collapse.js
+++ app/static/js/comment_collapse.js
@@ -2,55 +2,52 @@
     var toggle_button = "<span class='collapse_handle' style='cursor:pointer;margin-left:23px;margin-right:-26px;font-weight:bold;'>[-]</span>",
         $comments = $('div.comment');
 
-    //attach toggle button to each comment
     $comments.find('.comment_info').before(toggle_button);
 
-    $('.collapse_handle').on('click', function ( event ) {
-        var $self = $(this),
-            parent_offset = $self.parent().css('margin-left');
+    function expandOrCollapse(direction) {
+        var self = this,
+            parent_offset = self.parent().css('margin-left');
 
-        event.preventDefault();
-
-        //loop through each child, hide if offset is greater
-        $self.parent().nextAll().each( function ( i, e ) {
+        //loop through each child comment, hide/show if offset is greater
+        self.parent().nextAll().each( function ( i, e ) {
             var current_offset = $(this).css('margin-left');
 
-            if ( current_offset > parent_offset ) {
-                $(this).slideUp();
+            if ( current_offset > parent_offset && $(this).css('display') != 'none') {
+                ( direction == 'up' ) ? $(this).slideUp() : $(this).slideDown();
             } else {
                 return false; 
             }
-
-            $self.parent().css('margin-bottom',0).find('.comment_info').nextAll().hide();
-            $self.text('[+]').removeClass('collapse_handle').addClass('expand_handle');
-            $self.prev().hide();
-
         });
-    });
+    }
 
-    $('.expand_handle').on('click', function ( event ) {
-        console.log('f');
-        var $self = $(this),
-            parent_offset = $self.parent().css('margin-left');
+    function hideChildren() {
+        $self = $(this);
 
-        event.preventDefault();
+        expandOrCollapse.call( $self, 'up' );
 
-        $self.parent().nextAll().each( function ( i, e ) {
-            var current_offset = $(this).css('margin-left');
+        $self.text('[+]').addClass('expand_handle').removeClass('collapse_handle');
+        //hide comment body and reply/edit buttons
+        $self.parent().css('margin-bottom',0).find('.comment_info').nextAll().hide();
+        //hide voting arrows
+        $self.prev().hide();
 
-            if ( current_offset > parent_offset ) {
-                $(this).slideDown();
-            } else {
-                return false;
-            }
+        $self.off('click').on('click', showChildren);
+    }
 
-            $self.parent().css('margin-bottom',25).find('.comment_info').nextAll().show();
-            $self.text('[-]').addClass('collapse_handle').removeClass('expand_handle');
-            $self.prev().show();
+    function showChildren() {
+        $self = $(this);
 
-        });
+        expandOrCollapse.call( $self, 'down' );
 
-    });
+        $self.text('[-]').addClass('collapse_handle').removeClass('expand_handle');
+        //show comment body and buttons, reset margin
+        $self.parent().css('margin-bottom',25).find('.comment_info').nextAll().show();
+        //show votes
+        $self.prev().show();
 
+        $self.off('click').on('click', hideChildren);
+    }
+
+    $('span.collapse_handle').on('click', hideChildren );
 
 })( jQuery );
