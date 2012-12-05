a0d8b27074632311f2245396e1c59d0ae59bf460
Olaoluwa Osuntokun <laolu32@gmail.com>
2012-12-04 21:34:00 -0800

    Additional functions to facilitate AJAX comments

--- app/views/comment.py
+++ app/views/comment.py
@@ -1,5 +1,6 @@
 from flask import (Blueprint, url_for, render_template, session,
-                   request, redirect, g, jsonify, flash)
+                   request, redirect, g, jsonify, flash, make_response,
+                   current_app)
 
 from ..decorators import login_required
 from ..models.user import User
@@ -11,6 +12,7 @@ mod = Blueprint('comment', __name__, url_prefix='/comment')
 
 
 @mod.route('/<int:comment_id>/delete', methods=['DELETE', 'POST'])
+@login_required
 def delete_comment(comment_id):
 
     origin_post = api.delete_post_comment(comment_id)
@@ -20,6 +22,7 @@ def delete_comment(comment_id):
 
 
 @mod.route('/<int:comment_id>/edit', methods=['POST'])
+@login_required
 def edit_comment(comment_id):
     updated_comment = request.form['new_comment']
     api.edit_comment(comment_id, updated_comment)
@@ -33,6 +36,27 @@ def edit_comment(comment_id):
 def view_comment(comment_id):
     perma_comment = api.get_single_comment(comment_id)
     post = api.get_single_post(via_comment=perma_comment)
+    #get context for comment
     context_tree = api.get_post_comments(post[0], context_id=perma_comment.key.id())
 
     return render_template("post.html", content=post, comments=context_tree)
+
+
+@mod.route('/_newest', methods=['GET'])
+def latest_comment():
+    user = g.user
+    comment = Comment.query().filter(Comment.author == user.key).order(-Comment.date_created).get()
+    comment.depth = 0
+    comment.children = []
+
+    render_comment = get_macro_with_context('/macros/_render_comments.html', 'render_comments')
+    latest_comment = render_comment([comment])
+    resp = make_response(latest_comment)
+    return resp
+
+
+def get_macro_with_context(template_name, macro_name):
+    template = current_app.jinja_env.get_template(template_name)
+    module = template.make_module({'session': session, 'g': g})
+    macro = getattr(module, macro_name)
+    return macro
